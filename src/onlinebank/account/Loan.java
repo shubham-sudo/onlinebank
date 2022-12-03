5600f242d8608ab754e35d266119ddf75af04d93
魏 <weitf@bu.edu>
2022-12-03 16:28:04 -0500

    update on accounts

--- src/onlinebank/account/Loan.java
+++ src/onlinebank/account/Loan.java
@@ -1,4 +1,13 @@
 package onlinebank.account;
 
+import java.util.ArrayList;
 public interface Loan {
+    public string name;
+    public string password;
+    public ArrayList<Collateral> collaterals;
+    public ArrayList<Currency> debt;
+    public boolean PayFinished(Currency c);
+    public ArrayList<Currency> viewDebt();
+    public void printDebt();
+    public void getInterest(float InterestRate);
 }
--- src/onlinebank/account/Transaction.java
+++ src/onlinebank/account/Transaction.java
@@ -1,4 +1,11 @@
 package onlinebank.account;
 
 public interface Transaction {
+    public void saveMoney(SavingAccount account,Currency c);
+    public void saveMoney(CheckingAccount account,Currency c,float FeeRate);
+
+    public boolean WithdrawMoney(SavingAccount account,Currency c);
+    public boolean WithdrawMoney(CheckingAccount account,Currency c,float FeeRate);
+
+    public boolean PayLoan(Loan loan, Currency c);
 }
